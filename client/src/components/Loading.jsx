160f0dc3f6981a8963bb86fa2dbc4251facf7d05
SadanandMiskin <miskinsadanand@gmail.com>
2024-10-19 22:47:56 +0530

    loading fix

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,20 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import '../styles.css'
 function Loading() {
-const [message , setMessage] = useState('Connecting to server...')
-const [newMessage, setNewMessage] = useState('Please wait')
+const [message , setMessage] = useState('Connecting to Socket server...')
+const [newMessage, setNewMessage] = useState('Initally it takes ~40seconds')
+const [count , setCount] = useState(0)
   useEffect(()=>{
-    setTimeout(()=>{
-      setMessage('Connection Failed :(')
-      setNewMessage('Try again... ')
-      
-    }, 20000)
-  })
+
+    const intervalOut = setInterval(()=>{
+      setCount((prevC) => prevC + 1)
+    },1000)
+   const timeOut =  setTimeout(()=>{
+      setMessage('Working on it...')
+      setNewMessage('Taking more time than usual. ')
+    }, 30000)
+    const timeOut1 =  setTimeout(()=>{
+      setMessage('Almost done.')
+      setNewMessage('Sit back relax :)')
+    }, 50000)
+
+    return () => {
+      clearInterval(intervalOut)
+      clearTimeout(timeOut)
+      clearTimeout(timeOut1)
+    }
+  }, [])
   return (
     <div className="loading-container">
       <div className="loader"></div>
       <p>{message}</p>
       <p> {newMessage} </p>
+      <p>{count} s</p>
     </div>
   );
 }
